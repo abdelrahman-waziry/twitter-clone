2810737d4e1e5200ac81f707e3995edaa52f3db9
abdelrahman-waziry <abdelrahman.waziry@gmail.com>
2018-11-28 00:08:36 +0200

    Social login component

---
+++ resources/views/auth/social.blade.php
@@ -0,0 +1,17 @@
+<div class="button-group text-center">
+    <button type="submit" class="btn btn-primary social-btn twitter">
+        <a class="btn btn-link" href="{{ url('/login/twitter') }}">
+            Login with Twitter
+        </a>
+    </button>
+    <button type="submit" class="btn btn-primary social-btn facebook">
+        <a class="btn btn-link" href="{{ url('/login/facebook') }}">
+            Login with Facebook
+        </a>
+    </button>
+    <button type="submit" class="btn btn-primary social-btn google">
+        <a class="btn btn-link" href="{{ url('/login/google') }}">
+            Login with Google
+        </a>
+    </button>
+</div>
