abbc6aab56937d6e7788905ed4f7939b63a894fa
abdelrahman-waziry <abdelrahman.waziry@gmail.com>
2018-11-28 00:01:49 +0200

    Implement Tweet model relations

--- app/Tweet.php
+++ app/Tweet.php
@@ -6,5 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tweet extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'body', 'user_id',
+    ];
+
+    protected $appends = ['likes', 'subject'];
+
+    public function reaction()
+    {
+        return $this->belongsTo('App\Reaction', 'id');
+    }
+
+    public function subject()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    public function getLikesAttribute()
+    {
+        $likes = $this->reaction()->count();
+        return $likes;
+    }
+
+    public function getSubjectAttribute()
+    {
+        $subject = $this->subject()->first();
+        return $subject;
+    }
 }
