257f8e48f10a6aeaa67bfea15e37dce43ae5fd34
abdelrahman-waziry <abdelrahman.waziry@gmail.com>
2018-11-28 00:09:16 +0200

    Adds main routes

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
-
+ */
 
 Auth::routes();
 
@@ -21,6 +20,19 @@ Auth::routes();
 Route::get('login/{provider}', 'Auth\LoginController@redirectToProvider');
 Route::get('auth/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
 
-Route::group(['middleware'=>'auth'], function(){
-	Route::get('/', 'TweetsController@index');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', 'TweetsController@index');
+
+    /**
+     * Tweets related routes
+     */
+
+    Route::post('tweet/create', 'TweetsController@store');
+
+    /**
+     * User related routes
+     */
+
+    Route::get('user/{name}', 'UsersController@show');
+
 });
