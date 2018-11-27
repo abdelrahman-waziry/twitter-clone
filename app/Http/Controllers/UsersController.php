853a5157156cdb58d31770aa4b8b4cd25d845e62
abdelrahman-waziry <abdelrahman.waziry@gmail.com>
2018-11-28 00:10:25 +0200

    Handles user routes requests

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\UserRepository;
+
+class UsersController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->users = new UserRepository;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  string  $username
+     * @return \Illuminate\Http\Response
+     */
+    public function show(string $username)
+    {
+        $user = $this->users->userByName($username);
+        $tweets = $this->users->tweetsByUser($user->id);
+        
+        $data = [
+            'user' => $user,
+            'tweets' => $tweets
+        ];
+        
+
+        return view('profile', $data);
+    }
+}
